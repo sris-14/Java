a300f6057c1e972688817fdc4fe4937d39866d39
Srishti Shukla <srishtimini14@gmail.com>
2024-03-10 12:08:45 +0530

    changes

---
+++ 8.Arrays/MaxSubarraySum3.java
@@ -0,0 +1,40 @@
+public class MaxSubarraySum3 {
+
+    public static void kadanes(int arr[]){
+      int ms = Integer.MIN_VALUE;
+      int cs = 0;
+    //   int minVal = Integer.MIN_VALUE;
+      for(int i =0 ; i< arr.length;i++){
+        if(arr[i]<0){
+          if(arr[i]> ms){
+            ms = arr[i];
+          }
+        }
+
+        // else{
+        //     cs = cs + arr[i];
+        // if(cs < 0){
+        //   cs = 0;
+        // }
+        // ms = Math.max(cs , ms);
+        // }
+      }
+
+
+      for(int i=0;i<arr.length;i++){
+        cs = cs + arr[i];
+        if(cs < 0){
+          cs = 0;
+        }
+        ms = Math.max(cs , ms);
+      }
+
+      System.out.println("Our maximum subarray sum is : "+ms);
+    }
+
+    public static void main(String args[]){
+     int arr[] = {-2, -3, -1, -2, -3};
+    // int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+     kadanes(arr);
+    }
+}
