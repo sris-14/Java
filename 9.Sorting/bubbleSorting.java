732c4ef33f3c4fca660878fc6871943c47eb93cd
Srishti Shukla <srishtimini14@gmail.com>
2024-03-13 23:56:51 +0530

    change

--- 9.Sorting/bubbleSorting.java
+++ 9.Sorting/bubbleSorting.java
@@ -18,7 +18,7 @@ public class bubbleSorting{
      }
 
      public static void main(String args[]){
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {3,5,1,6,7};
         bubbleSort(arr);
         printArr(arr);
      }
