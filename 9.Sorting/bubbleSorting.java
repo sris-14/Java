54a31a436aa564695b34d024ee6d4e41296d9b3c
Srishti Shukla <srishtimini14@gmail.com>
2024-03-13 23:59:07 +0530

    change

--- 9.Sorting/bubbleSorting.java
+++ 9.Sorting/bubbleSorting.java
@@ -2,12 +2,17 @@ public class bubbleSorting{
 
      public static void bubbleSort(int arr[]){
          for(int i =0; i< arr.length-1;i++){
+            int swap =0;
             for(int j= 0; j< arr.length-1-i; j++){
                if(arr[j] > arr[j+1]){
                   int temp= arr[j];
                   arr[j] = arr[j+1];
                   arr[j+1] =temp;
+                  swap ++;
                }
+            }
+            if(swap ==0){
+               System.out.println("array is already sorted!");
             }
          }
      }
@@ -18,7 +23,7 @@ public class bubbleSorting{
      }
 
      public static void main(String args[]){
-        int arr[] = {3,5,1,6,7};
+        int arr[] = {1, 2, 3, 4, 5};
         bubbleSort(arr);
         printArr(arr);
      }
