f8a31c6e6a89aeff7455825be58862918d299683
Srishti Shukla <srishtimini14@gmail.com>
2024-03-14 23:19:28 +0530

    change

--- 9.Sorting/SelectionSort.java
+++ 9.Sorting/SelectionSort.java
@@ -20,7 +20,7 @@ public class SelectionSort {
         }
     }
     public static void main(String args[]){
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {5, 4, 1, 3, 2, 9};
         selectionSort(arr);
         printArr(arr);
     }
