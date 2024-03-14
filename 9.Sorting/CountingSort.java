a5d3a64b5465019efcf4a69a2e34a16b1f0f00d2
Srishti Shukla <srishtimini14@gmail.com>
2024-03-14 23:21:40 +0530

    add

--- 9.Sorting/CountingSort.java
+++ 9.Sorting/CountingSort.java
@@ -1,3 +1,5 @@
 public class CountingSort {
-    
+    public static void main(String args[]){
+        
+    }
 }
--- 9.Sorting/InbuiltSort.java
+++ 9.Sorting/InbuiltSort.java
@@ -1,3 +1,5 @@
 public class InbuiltSort {
-    
+    public static void main(String args[]){
+        
+    }
 }
