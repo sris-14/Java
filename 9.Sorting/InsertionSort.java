6ab190d3fac760efb58369f2f95ad46ae527ceab
Srishti Shukla <srishtimini14@gmail.com>
2024-03-14 23:18:38 +0530

    update

--- 9.Sorting/InsertionSort.java
+++ 9.Sorting/InsertionSort.java
@@ -20,7 +20,7 @@ public class InsertionSort {
     }
 
     public static void main(String args[]){
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {5, 4, 1, 3, 9};
         insertionSort(arr);
         printArr(arr);
     }
