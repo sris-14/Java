fc57f773e1b8eb49a461085b6b6ddb14d6e8af55
Srishti Shukla <srishtimini14@gmail.com>
2024-03-16 23:18:25 +0530

    add

---
+++ 11.Strings/Basics.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Basics {
+    public static void main(String args[]){
+        char arr[] ={'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        // Strings are IMMUTABLE (not changeable)
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name =sc.nextLine();
+        System.out.println(name);
+// String length
+        System.out.println(name.length());
+        // string concatination
+        String firstName = "Srishti";
+        String lastName = "Shukla";
+        System.out.println(firstName + lastName);
+    }
+}
