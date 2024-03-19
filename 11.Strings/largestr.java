a4056f2f2d30c56ff68b871f39cca2b92b470c46
Srishti Shukla <srishtimini14@gmail.com>
2024-03-19 07:42:38 +0530

    add

--- 11.Strings/largestr.java
+++ 11.Strings/largestr.java
@@ -5,10 +5,20 @@ public class largestr {
 
         String largest = fruits[0];
         for(int i=0; i<fruits.length; i++){
+            /*
+            here largest have "apple" 
+            when i =0, fruits[i]="apple"
+            largest = fruits[0] as .compareTos gives 0
+            when i=1, fruits[1] = "mango"
+            largest.comapreTo(fruits[1]) give -ve
+            str1< str2 i.e. apple < mango
+            hence largest = mango
+             */ 
             if(largest.compareTo(fruits[i]) < 0){
                 largest = fruits[i];
             }
         }
+        // TC: O(x*N)
         System.out.println(largest);
     }
 }
