c7375d5ce33d2681ea26253961a6d79af3efc79e
Srishti Shukla <srishtimini14@gmail.com>
2024-03-22 20:42:10 +0530

    add

--- 12.Bit Manipulation/BitManipulation.java
+++ 12.Bit Manipulation/BitManipulation.java
@@ -1,5 +1,18 @@
 public class BitManipulation{
     public static void main(String args[]){
-        System.out.print((5 & 6));
+        // AND operator
+        System.out.println((5 & 6));
+        // OR operator
+        System.out.println((5 | 6 ));
+        // XOR operator
+        System.out.println((5 ^ 6));
+        // Binary one's complement
+        System.out.println((~5));
+        System.out.println((~0));
+        // Binary left shift
+        System.out.println((5 << 2));
+        // Binary right shift
+        System.out.println((5 >> 2));
+        System.out.println((6 >> 1));
     }
 }
