b0db33b725baface407305a681f049c89777d5b2
Srishti Shukla <srishtimini14@gmail.com>
2024-03-22 22:02:56 +0530

    add

--- 12.Bit Manipulation/BitOperations.java
+++ 12.Bit Manipulation/BitOperations.java
@@ -12,8 +12,23 @@ public class BitOperations {
         int bitMask = 1<<i;
         return n|bitMask;
     }
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<< i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit){
+        if(newBit == 0 ){
+            return clearIthBit(n, i);
+        } else {
+            return setIthBit(n, i);
+        }
+    }
     public static void main(String args[]){
-       System.out.println(getIthBit(10, 3));
-       System.out.println(setIthBit(11, 0));
+       System.out.println("getIthBit : "+getIthBit(10, 3));
+       System.out.println("setIthBit : "+setIthBit(10, 2));
+       System.out.println("clearIthBit : "+clearIthBit(10, 1));
+
+       System.out.println("upadte ith bit : " +updateIthBit(0, 0, 0));
     }
 }
