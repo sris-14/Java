a0072faecb4156a22fca1e78223b8fd6f3c8d386
Srishti Shukla <srishtimini14@gmail.com>
2024-03-24 00:43:22 +0530

    add

--- 12.Bit Manipulation/practice.java
+++ 12.Bit Manipulation/practice.java
@@ -1,8 +1,12 @@
 public class practice {
     public static void main(String args []){
-        // swap two numbes without using third variable.
+        // swap two numbers using XOR
         int a = 5;
         int b = 2;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
         System.out.println(a);
+        System.out.println(b);
     }
 }
