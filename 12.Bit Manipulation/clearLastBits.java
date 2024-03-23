98dd9eace91130dc9f0b9f6955cc1ad477562e07
Srishti Shukla <srishtimini14@gmail.com>
2024-03-23 21:00:14 +0530

    add

--- 12.Bit Manipulation/clearLastBits.java
+++ 12.Bit Manipulation/clearLastBits.java
@@ -1,9 +1,10 @@
 public class clearLastBits {
 
-    public static void clearLastIthBits(int n){
-        
+    public static int clearLastIthBits(int n, int i){
+        int bitMask = (~0) << i;
+        return n & bitMask;
     }
     public static void main(String args[]){
-
+         System.out.println(clearLastIthBits(15, 2));
     }
 }
