398f8631861961550fcb94545f94e405d21e5a05
Srishti Shukla <srishtimini14@gmail.com>
2024-03-24 00:40:24 +0530

    add

---
+++ 12.Bit Manipulation/practice.java
@@ -0,0 +1,8 @@
+public class practice {
+    public static void main(String args []){
+        // swap two numbes without using third variable.
+        int a = 5;
+        int b = 2;
+        System.out.println(a);
+    }
+}
