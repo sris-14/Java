d2997a18601e6effd584d940aa6afe75a06d0d7e
Srishti Shukla <srishtimini14@gmail.com>
2024-03-26 13:13:30 +0530

    add

--- 13.OOPS/OOPS.java
+++ 13.OOPS/OOPS.java
@@ -1,7 +1,7 @@
 public class OOPS{
 
     // access specifier: static
-    // by convention main function should be static and public 
+    
     public static void main(String args[]){
         Pen p1 = new Pen(); //created a pen object called p1
         // p1.setColor("Blue");
@@ -11,48 +11,27 @@ public class OOPS{
         p1.tip = 5;
         System.out.println(p1.tip);
 
-        BankAccount myAcc= new BankAccount();
-        myAcc.username = "srishti";
-        myAcc.setPassword("abcd");
     }
     
 }
-// Blueprint of class
-
-class BankAccount{
-    public String username;
-    private String password; 
-
-    public void setPassword(String pwd){
-        password = pwd;
-    }
-
-}
-
-// multiple classes can be made
-// by convention classes name start with capital letter
 
 // blueprint of pen
 class Pen{
     String color;
     int tip;
+
+    // getter -
+    String getColor(){
+        return this.color;
+    }
    
+    // the below are setters : setting the color
     void setColor(String newColor){
         color = newColor;
     }
-
+//  setter which set the tip of a pen
     void setTip(int newTip){
         tip= newTip;
     }
 }
 
-// blueprint of student
-class Student {
-    String name;
-    int age;
-    float percentage;
-
-    void calcPercentage(int phy, int chem, int math){
-        percentage = (phy + chem + math)/3;
-    }
-}
