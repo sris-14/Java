9002a03d97b38e9d317ea6eec25c4fce87ef153c
Srishti Shukla <srishtimini14@gmail.com>
2024-04-03 23:44:21 +0530

    add

---
+++ 16.Backtracking/Classroom.java
@@ -0,0 +1,14 @@
+public class Classroom {
+    public static void changeArr(int arr[], int i){
+
+    }
+
+    public static void printArr(int arr[]){
+        
+    }
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
