76b3fd766ae799304e193c13c615c7bc71e95f5a
Srishti Shukla <srishtimini14@gmail.com>
2024-04-04 06:50:55 +0530

    add

---
+++ 16.Backtracking/Classroom2.java
@@ -0,0 +1,21 @@
+public class Classroom2 {
+    public static void findSubsets(String str,String ans, int i){
+        //Base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            }
+          System.err.println(ans + " ");
+          return;
+        }
+        //recursion
+        //Yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}
