9b051d4ea23a8d1e8eff608eed829e594c26a8f0
Srishti Shukla <srishtimini14@gmail.com>
2024-04-04 06:30:27 +0530

    add

--- 16.Backtracking/Classroom.java
+++ 16.Backtracking/Classroom.java
@@ -1,10 +1,21 @@
 public class Classroom {
-    public static void changeArr(int arr[], int i){
-
+    public static void changeArr(int arr[], int i, int val){
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursion
+        arr[i] = val;
+        changeArr(arr, i+1, val+1);//func call
+        arr[i] = arr[i] - 2; //Backtracking step
     }
 
     public static void printArr(int arr[]){
-        
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
     public static void main(String args[]){
         int arr[] = new int[5];
