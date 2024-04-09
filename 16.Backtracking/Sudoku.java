b211b4d6a7fb7634e255a5745e2576f5c1620b30
Srishti Shukla <srishtimini14@gmail.com>
2024-04-09 22:03:28 +0530

    add

--- 16.Backtracking/Sudoku.java
+++ 16.Backtracking/Sudoku.java
@@ -1,7 +1,36 @@
 public class Sudoku {
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit){
+        // column
+        for(int i=0; i<9; i++){
+            if(sudoku[i][col] == digit){
+                return false;
+            }
+        }
+
+        //row
+        for(int j=0; j<9; j++){
+             if(sudoku[row][j] == digit){
+                return false;
+             }
+        }
+
+        //grid
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+        for(int i =sr; i<sr+3; i++ ){
+            for(int j=sc; j<sc+3; j++){
+                if(sudoku[i][j] == digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     public static boolean sudokuSolver(int sudoku[][], int row, int col){
         //base case
-
+        if(row == 9){
+            return true;
+        }
         //recursion
         int nextRow = row, nextCol = col+1;
         if(col+1 == 9){
@@ -18,7 +47,18 @@ public class Sudoku {
                 if(sudokuSolver(sudoku, nextRow, nextCol)){
                    return true;
                 }
+                sudoku[row][col] = 0;
             }
+        }
+        return false;
+    }
+
+    public static void printSudoku(int sudoku[][]){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print(sudoku[i][j] + " ");
+            } 
+            System.out.println();
         }
     }
         public static void main(String args[]){
@@ -31,6 +71,12 @@ public class Sudoku {
         {0, 3, 0, 0, 7, 2, 0, 0, 4},
         {0, 4, 9, 0, 3, 0, 0, 5, 7},
         {8, 2, 7, 0, 0, 9, 0, 1, 3} };
-
+    
+         if(sudokuSolver(sudoku, 0, 0)){
+            System.out.println("Solution exists.");
+            printSudoku(sudoku);
+         } else {
+            System.out.println("Solution does not exist.");
+         }
     }
 }
