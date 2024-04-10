2582db47e77f7eec9c353b18688a16337665f286
Srishti Shukla <srishtimini14@gmail.com>
2024-04-10 23:59:29 +0530

    add

---
+++ 17.Arraylists/Basics.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Basics{
+    public static void main(String args[]){
+        // ClassName objectName = new Classroom
+        ArrayList<Integer>list = new ArrayList<>();
+
+    }
+}
