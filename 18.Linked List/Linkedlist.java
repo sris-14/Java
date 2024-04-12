80e28464cf37a3dfa6e44153cf0e6a6ec538d33e
Srishti Shukla <srishtimini14@gmail.com>
2024-04-13 00:17:39 +0530

    add

---
+++ 18.Linked List/Linkedlist.java
@@ -0,0 +1,14 @@
+public class Linkedlist{
+
+    public static class Node{
+        int data;
+        Node next;//refernce var
+        public Node(int data){// constructor : paramter constructor
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String args[]){
+
+    }
+}
